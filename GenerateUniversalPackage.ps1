a3664ab509faded9fcd3106294294285e20e9ef4
freddydk <freddydk@users.noreply.github.com>
2024-01-14 08:28:13 +0100

    auth

--- GenerateUniversalPackage.ps1
+++ GenerateUniversalPackage.ps1
@@ -7,6 +7,20 @@ if ($artifactType -eq '') { $artifactType = 'sandbox' }
 $artifactVersion = $env:artifactVersion
 $package = $env:package
 
+# https://dev.azure.com/freddydk/apps/_artifacts/feed/universal
+$feedUrl = $env:feedUrl
+if ($feedUrl -match '^(https:\/\/dev\.azure\.com\/[^\/]+\/)([^\/]+)\/_artifacts\/feed\/([^\/]+)$') {
+    $organization = $matches[1]
+    $project = $matches[2]
+    $feed = $matches[3]
+}
+else {
+    throw "Invalid feedUrl '$feedUrl'"
+}
+$feedToken = $env:feedToken
+
+Write-Output $feedToken | az devops login --organization $organization
+
 $artifactUrl = Get-BcArtifactUrl -type $artifactType -version $artifactVersion -country $package
 $folders = Download-Artifacts -artifactUrl $artifactUrl -includePlatform:($package -eq 'base')
 
