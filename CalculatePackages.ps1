e8931d0167912e17abd24d62c072bda811a04ff4
freddydk <freddydk@users.noreply.github.com>
2024-01-15 13:54:35 +0100

    add country

--- CalculatePackages.ps1
+++ CalculatePackages.ps1
@@ -5,10 +5,11 @@ Write-Host "Calculate Packages"
 $artifactType = $env:artifactType
 if ($artifactType -eq '') { $artifactType = 'sandbox' }
 $artifactVersion = $env:artifactVersion
+$country = $env:country
 if (([System.Version]$artifactVersion).Revision -eq -1) { throw "Invalid artifactVersion '$artifactVersion'" }
 
 $artifactUrls = Get-BcArtifactUrl -type $artifactType -version $artifactVersion -select all
-$packages = $artifactUrls | ForEach-Object { @{ "package" = "$_".Split('/')[5] } }
+$packages = $artifactUrls | Where-Object { "$_".Split('/')[5] -like $country } | ForEach-Object { @{ "package" = "$_".Split('/')[5] } }
 
 Write-Host "Packages:"
 ConvertTo-Json -InputObject @($packages) -Compress | Out-Host
